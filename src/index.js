02d8a148960fd1364974d4f1eff46ef3abcfe86d
Dan Steshenko <dan.steshenko@gmail.com>
2017-10-07 00:00:00 +0300

    Trying to figure out how react/redux works

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './styles/styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+      <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
