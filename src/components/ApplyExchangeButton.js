dcbdaf0c46f9bb19d0385284a6698579aea6981c
Dan Steshenko <dan.steshenko@gmail.com>
2017-10-14 18:22:19 +0300

    Add propTypes to exchange button

--- src/components/ApplyExchangeButton.js
+++ src/components/ApplyExchangeButton.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
 import ApplyExchangeWrapper from '../containers/ApplyExchangeWrapper'
+import PropTypes from 'prop-types'
 
 class ApplyExchangeButton extends Component {
   render() {
+    const {
+      moneyFrom,
+      moneyTo,
+      applyExchange
+    } = this.props;
+
     return (
       <button
-        disabled={!this.props.moneyFrom || !this.props.moneyTo}
+        disabled={!moneyFrom || !moneyTo}
         className="apply-exchange" 
-        onClick={this.props.applyExchange}>
+        onClick={applyExchange}>
           Exchange
       </button>
     )
   }
+}
+
+ApplyExchangeButton.propTypes = {
+  moneyFrom: PropTypes.number,
+  moneyTo: PropTypes.number,
+  applyExchange: PropTypes.func
 }
 
 export default ApplyExchangeWrapper(ApplyExchangeButton);
